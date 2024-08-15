769719355ad60d358815afb0795e9e7fc834a490
Aashray Munjal <aashraymunjal@gmail.com>
2024-08-14 21:21:51 -0400

    adding quiz creation changes - backend

---
+++ src/main/java/com/example/demo/model/Quiz.java
@@ -0,0 +1,54 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Quiz {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	int id;
+	@Column
+	String title;
+	@Column
+	@ManyToMany
+	List<Question> questions;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+
+	@Override
+	public String toString() {
+		return "Quiz [id=" + id + ", title=" + title + ", questions=" + questions + "]";
+	}
+
+}
